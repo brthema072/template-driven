1160561546fcf058cdaeeb56c71dc169950ba5cf
brthema072 <brunomarthe@hotmail.com>
2020-01-15 23:33:51 -0300

    validação nativa do angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { FormNativeValidationComponent } from './form-native-validation/form-native-validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent
+    TemplateDrivenFormComponent,
+    FormNativeValidationComponent
   ],
   imports: [
     BrowserModule,
